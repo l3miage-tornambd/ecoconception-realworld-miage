5a47be0f06f1907e332eace2ed9ccb19efc344ef
TorDamien <tornambe.damien@gmail.com>
2025-01-06 11:30:01 +0100

    BP 46/47 Limiter l'utilisation des GIFs animé / limiter les vidéos en auto play

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,14 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { TokenInterceptor } from "./core/interceptors/token.interceptor";
 import { ErrorInterceptor } from "./core/interceptors/error.interceptor";
 import { ApiInterceptor } from "./core/interceptors/api.interceptor";
+import { VideoPlayerComponent } from "./video-player/video-player.component";
 
 export function initAuth(jwtService: JwtService, userService: UserService) {
   return () => (jwtService.getToken() ? userService.getCurrentUser() : EMPTY);
 }
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, VideoPlayerComponent],
   imports: [
     BrowserModule,
     FooterComponent,
